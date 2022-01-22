5b981eda5fc51ba70c8d0310d96187b56dc15bf9
Valdecir Neumann <56264244+PiahDoNeumann@users.noreply.github.com>
2022-01-22 17:05:16 -0300

    First Commit

---
+++ src/components/ParceiroCasa.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="casa">
+    <h1>Por que a CasaMot - Motilidade em Peixes?</h1>
+    <p>
+      Estudo de Sistema Casa (Computer-assisted sperm analysis ou Analise espermatica assistida por computador) para Peixes.
+    </p>
+    <p>
+      <a href="https://github.com/PiahDoNeumann/mj-21">Material disponivel</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ParceiroCasa',
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
